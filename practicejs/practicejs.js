1f12b2607c4ccf6eba21ad9fede67b6f5c4fccaf
Maheshwaran-mm24 <mm24mahesh@gmail.com>
2025-12-26 20:50:20 +0530

    Add files via upload

---
+++ practicejs/practicejs.js
@@ -0,0 +1,87 @@
+// Question1
+let a = "10"
+let b = 5
+let c = a+b
+console.log(c)
+console.log(typeof c)
+
+// Question2
+let x = 10
+let y = "10"
+console.log(x==y)
+console.log(x===y)
+
+// Question3
+let num = 15
+let result = (num>=0)?"positive":"Negative"
+console.log(result)
+
+//Question4
+let p = 10
+let q = 20
+let r = 30
+let res = p+q+r
+console.log(res)
+console.log(typeof res)
+
+// Question5
+let num1 = true
+let res1 = Number(true) + 5
+console.log(res1)
+console.log(typeof res1)
+
+
+// Question6
+let a = 7
+let b = 3
+let c = a-b
+let d = (c>0) ? "a is greater than b":" b is greater than a"
+let res = (a>b)
+console.log(d)
+console.log(res)
+
+
+// Question7
+let marks = "85"
+let total = (marks>=40) ? "Passed":"Failed"
+console.log(total)
+console.log(typeof total)
+
+
+// Question8
+let age = 20
+let id = false
+let res = (age>=18 && id==true)
+console.log(res)
+
+
+// Question9
+let count
+console.log(count)
+console.log(typeof count)
+count=10
+console.log(count)
+console.log(typeof count)
+
+
+// Question10
+let num1 = "5"
+let num2 = "2"
+let add = num1+num2
+let sub = num1-num2
+let mul = num1*num2
+console.log(add,typeof add)
+console.log(sub,typeof sub)
+console.log(mul,typeof mul)
+
+
+
+
+
+
+
+
+
+
+
+
